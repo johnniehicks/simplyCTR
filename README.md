f753bf1ea9ac569c54a057258d00396acc79e9e0
JergenJones <jergenjones@gmail.com>
2016-08-08 16:51:08 -0500

    udpate readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # simplyCTR
+
+### Run
+`npm install`
+`brunch watch -s` localhost:3333 by default
+`brunch build`
---
+++ app/application.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var $ = require('jquery');
+
+var App = {
+  init: function init() {
+    console.log('App initialized.');
+  }
+};
+
+/*var App = {
+  init: function init() {
+    $('body').append('App initialized.');
+  }
+};*/
+
+module.exports = App;
